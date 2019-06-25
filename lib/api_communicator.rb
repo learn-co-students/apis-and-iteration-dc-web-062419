24d655d610578e477768b81cc4a9f6555fb57bf0
github username <github email address>
2019-06-25 17:41:49 -0400

    shortened to 36 lines

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,11 +5,7 @@ require 'pry'
 # Returning films for given character
 def films(character, response_hash)
   films_list = []
-  response_hash["results"].each do |thing| 
-    if thing["name"].downcase == character.downcase
-      films_list = thing["films"] 
-    end
-  end
+  response_hash["results"].each { |thing| thing["name"].downcase == character.downcase ?  films_list = thing["films"] : nil }
   films_list
 end
 
@@ -22,17 +18,13 @@ end
 # Build collection of information on films
 def collect_the_films(urls)
   collected_film_info = {}
-  urls.each do |path|
-      collected_film_info[make_web_request(path)["title"]] = make_web_request(path)
-  end
+  urls.each { |path| collected_film_info[make_web_request(path)["title"]] = make_web_request(path) }
   movie_collection = collected_film_info
 end
 
 def print_movies(movie_collection)
   # some iteration magic and puts out the movies in a nice list
-  movie_collection.each do |k,v|
-    puts "#{k} was directed by #{v["director"]}"
-  end
+  movie_collection.each {|k,v| puts "#{k} was directed by #{v["director"]}" }
 end
 
 def get_character_movies_from_api(character)
@@ -43,10 +35,11 @@ def get_character_movies_from_api(character)
   print_movies(movie_collection)
 end
 
-def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
-end
+# def show_character_movies(character)
+#   get_character_movies_from_api(character)
+#   # films = get_character_movies_from_api(character)
+#   # print_movies(films)
+# end #this one isn't necessary if we just call the get_character_movies_from_api directly from the run.rb
 
 ## BONUS
 
