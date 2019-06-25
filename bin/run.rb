70374fddd43a2f4929e71e080e457695bb119708
Hannah Nilagate <Nilagathp@gmail.com>
2019-06-25 17:42:14 -0400

    All the work

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,9 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
-welcome
-character = get_character_from_user
-show_character_movies(character)
+
+welcome 
+user_input = get_character_from_user
+film_urls = find_films(user_input)
+puts print_movies(film_urls)
+# show_character_movies(character)
