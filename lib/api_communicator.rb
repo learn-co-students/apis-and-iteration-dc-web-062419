b2b77d1ab4245ec7d323199f2cca81433d64ac38
github username <github email address>
2019-06-25 16:38:30 -0400

    Lots of methods, almost an answer

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,13 +2,66 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+###helper method
+
+# Returning films for given character
+def films(character, response_hash)
+  films_list = []
+  response_hash["results"].each do |thing| 
+    if thing["name"].downcase == character.downcase
+      films_list = thing["films"] 
+    end
+  end
+  films_list
+end
+
+# Making a web request
+def make_web_request(path)
+  response_string = RestClient.get(path)
+  response_hash_movie = JSON.parse(response_string)
+end
+
+# Build collection of information on films
+def collect_the_films(urls)
+  collected_film_info = {}
+
+  urls.each do |path|
+      collected_film_info[make_web_request(path)["title"]] = make_web_request(path)
+      #puts response_hash_movie
+  end
+  collected_film_info
+end
+
+
+def print_movies(movie_collection)
+  # some iteration magic and puts out the movies in a nice list
+  binding.pry
+  collect_the_films(films).keys
+end
+
+def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+
+#   films = []
+
+#   response_hash["results"].each do |thing| 
+#     if thing["name"].downcase == character.downcase
+#       films = thing["films"] 
+#     end
+#   end
+#  #puts films
+
+  urls = films(character, response_hash)
+
+  collect_the_films(urls)
+  movie_collection = collect_the_films(urls)
+  print_movies(movie_collection)
+  #binding.pry
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -18,9 +71,9 @@ def get_character_movies_from_api(character_name)
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
-end
+#list_of_films = collect_the_films(urls)
+
+
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
@@ -31,3 +84,8 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+def z 
+x = method1
+y= method2(x) 
+
+end 
